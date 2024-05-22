4acf52208add62edb4287194a0211acc00441366
ychistel <ychistel@users.noreply.github.com>
2024-05-22 12:22:10 +0000

    deploy: 708f7c27f66dbf460e5e893f4f3cea75aa17a97c

--- _static/pyodide-worker.js
+++ _static/pyodide-worker.js
@@ -1,27 +1,40 @@
 // pyodide-worker.js
 
-self.importScripts('https://cdn.jsdelivr.net/pyodide/v0.22.0/full/pyodide.js');
-
-async function loadPyodideAndPackages() {
+// Vérifiez que nous sommes dans un contexte de WebWorker
+if (typeof importScripts !== 'undefined') {
     // Charger Pyodide
-    self.pyodide = await loadPyodide();
-    console.log("Pyodide loaded");
-}
+    importScripts('https://cdn.jsdelivr.net/pyodide/v0.22.0/full/pyodide.js');
 
-loadPyodideAndPackages();
-
-self.onmessage = async (event) => {
-    const pythonScript = event.data.python;
-
-    // Attendre que Pyodide soit prêt avant d'exécuter le script
-    await self.pyodide.loadPackagesFromImports(pythonScript);
-    let result;
-    try {
-        result = await self.pyodide.runPythonAsync(pythonScript);
-    } catch (error) {
-        result = `Error: ${error.message}`;
+    async function loadPyodideAndPackages() {
+        try {
+            self.pyodide = await loadPyodide();
+            console.log("Pyodide loaded");
+        } catch (error) {
+            console.error("Error loading Pyodide:", error);
+        }
     }
 
-    // Renvoyer le résultat au thread principal
-    self.postMessage({ result });
-};
+    loadPyodideAndPackages();
+
+    self.onmessage = async (event) => {
+        const pythonScript = event.data.python;
+
+        try {
+            await self.pyodide.loadPackagesFromImports(pythonScript);
+            let result;
+            try {
+                result = await self.pyodide.runPythonAsync(pythonScript);
+            } catch (error) {
+                result = `Error in Python script: ${error.message}`;
+            }
+
+            // Renvoyer le résultat au thread principal
+            self.postMessage({ result });
+        } catch (error) {
+            console.error("Error during Pyodide execution:", error);
+            self.postMessage({ result: `Error: ${error.message}` });
+        }
+    };
+} else {
+    console.error("importScripts is not defined. This script should be run in a WebWorker.");
+}
