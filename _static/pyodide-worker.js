25495620db56f249ba0bc02fb1b0cc616d442bee
ychistel <ychistel@users.noreply.github.com>
2024-05-24 04:20:49 +0000

    deploy: 2e09d9b1311da6c7b78ff14a247b3b8139b7b02e

--- _static/pyodide-worker.js
+++ _static/pyodide-worker.js
@@ -1,39 +1,26 @@
-// pyodide-worker.js
+self.importScripts('https://cdn.jsdelivr.net/pyodide/v0.22.0/full/pyodide.js');
 
-// Vérifiez que nous sommes dans un contexte de WebWorker
-if (typeof importScripts !== 'undefined') {
-    console.log("Running in a WebWorker context");
-    importScripts('https://cdn.jsdelivr.net/pyodide/v0.22.0/full/pyodide.js');
-
-    let pyodideReadyPromise = (async () => {
-        try {
-            self.pyodide = await loadPyodide();
-            console.log("Pyodide loaded successfully");
-        } catch (error) {
-            console.error("Error loading Pyodide:", error);
-        }
-    })();
-
-    self.onmessage = async (event) => {
-        const pythonScript = event.data.python;
-
-        await pyodideReadyPromise;  // Assurez-vous que Pyodide est chargé avant de traiter les messages
-
-        try {
-            await self.pyodide.loadPackagesFromImports(pythonScript);
-            let result;
-            try {
-                result = await self.pyodide.runPythonAsync(pythonScript);
-            } catch (error) {
-                result = `Error in Python script: ${error.message}`;
-            }
-
-            self.postMessage({ result });
-        } catch (error) {
-            console.error("Error during Pyodide execution:", error);
-            self.postMessage({ result: `Error: ${error.message}` });
-        }
-    };
-} else {
-    console.error("importScripts is not defined. This script should be run in a WebWorker.");
+async function initializePyodide() {
+    self.pyodide = await loadPyodide();
 }
+
+let pyodideReadyPromise = initializePyodide();
+
+self.onmessage = async (event) => {
+    await pyodideReadyPromise;
+
+    const { code } = event.data;
+
+    if (typeof code !== 'string') {
+        self.postMessage({ error: 'Le code reçu n\'est pas une chaîne de caractères.' });
+        return;
+    }
+
+    try {
+        let result = await self.pyodide.runPythonAsync(code);
+        console.log(code);
+        self.postMessage({ result });
+    } catch (error) {
+        self.postMessage({ error: error.message });
+    }
+};
